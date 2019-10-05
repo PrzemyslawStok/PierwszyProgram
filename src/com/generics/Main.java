c81343abad9039f8663556394126f782170e9f50
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-10-05 14:53:59 +0200

    niewielkie zmiany

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         String imie,nazwisko;
+        int wiek;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -15,13 +16,19 @@ public class Main {
 
         System.out.println("Wprowadzono: " + imie);
 
+        System.out.print("Wprowadź nazwisko: ");
         nazwisko = scanner.next();
 
         System.out.println("Wprowadzono: " + nazwisko);
 
+        System.out.print("wiek:");
+        wiek = Integer.parseInt(scanner.next());
+
+
         System.out.println("_____________________");
-        System.out.println("Imie:"+imie);
-        System.out.println("Kolejna linia");
+        System.out.println("Imie: "+imie);
+        System.out.println("Nazwisko: "+nazwisko);
+        System.out.println("Wiek: "+wiek);
         System.out.println("_____________________");
     }
 }
